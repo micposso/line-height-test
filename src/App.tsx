c3c4bc6083184e997422ebaecbb10d4cff6cf0a3
micposso <micposso@gmail.com>
2025-01-31 13:11:50 -0500

    add css and mock html

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './assets/styles.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="header-container">
+      <div className="card-container">
+        <div className="contract-type">
+          <p>Total in-force Contracts</p>
+        </div>
+        <div className="contract-amount">50</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="card-container">
+        <div className="contract-type">
+          <p>Pending Contracts</p>
+        </div>
+        <div className="contract-amount">4</div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="card-container">
+        <div className="contract-type">
+          <p>Death Pending Contracts</p>
+        </div>
+        <div className="contract-amount">1</div>
+      </div>
+    </div>
+  );
 }
 
 export default App
